72c190c352be3870e3b0f99b5b1fc0016223604e
Crzidea <i@crzidea.com>
2017-06-14 11:17:29 +0800

    Add infos

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let render
 fetch(`${API_ROOT}/coinm.json`)
 .then(response => response.json())
 .then((data) => {
-  console.log(data);
   data.leader = data.bots[0]
   data.leader.statistics.info.funds.freezed.cny =
     Number(data.leader.statistics.info.funds.freezed.cny).toFixed(2)
@@ -13,6 +12,7 @@ fetch(`${API_ROOT}/coinm.json`)
     Number(data.leader.statistics.info.funds.free.cny)
     + data.bots.reduce((sum, bot) => sum + bot.allocated, 0)
   ).toFixed(2)
+  const date = new Date(data.date)
   for (const bot of data.bots) {
     shiftField('free')
     shiftField('freezed')
@@ -20,7 +20,13 @@ fetch(`${API_ROOT}/coinm.json`)
     bot.allocated = bot.allocated.toFixed(2)
     bot.available = bot.available.toFixed(2)
     bot.total     = bot.total.toFixed(2)
-    bot.statistics.orders = bot.statistics.orders.reverse()
+    bot.statistics.rankRate = bot.statistics.rankRate.toFixed(2)
+    bot.statistics.orders   = bot.statistics.orders.reverse()
+    for (const order of bot.statistics.orders) {
+      order.push('sell' === order[0])
+      order[0] = order[0].toUpperCase()
+      order[4] = ((date - (new Date(order[4]))) / 3600000).toFixed(1) + 'h'
+    }
     function shiftField(field) {
       bot[field] =
         Number(bot.statistics.info.funds[field][bot.options.token])
